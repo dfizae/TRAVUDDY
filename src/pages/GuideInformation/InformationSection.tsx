320fc8cb8decfce75b2610d525e86b49d60775f0
jaeyoungkim <gaza1268@naver.com>
2026-05-01 15:28:52 +0900

    fix: GuideInformation 페이지  informationSection 부분 데톱 디자인  수정

--- src/pages/GuideInformation/InformationSection.tsx
+++ src/pages/GuideInformation/InformationSection.tsx
@@ -16,17 +16,19 @@ export default function InformationSection() {
     return (
         <>
             {/* toggle 버튼 섹션 */}
-            <div className="flex gap-3 items-center justify-center px-4 py-5 mb-5">
-                <button className="flex flex-col gap-1 items-center border-b px-12" onClick={() => handleToggleClick("course")}>
-                    <img src={isClicked ? "/images/icons/ico-course_active.svg" : "/images/icons/ico-course_default.svg"} alt="Course" />
-                    <span className={`text-xs pb-0.5 font-semibold ${isClicked ? "text-black-600" : "text-gray-500"}`}>3·Course</span>
-                </button>
-                <button className="flex flex-col gap-1 items-center border-b px-12" onClick={() => handleToggleClick("review")}>
-                    <img src={isClicked ? "/images/icons/ico-review_default.svg" : "/images/icons/ico-review_active.svg"} alt="Review" />
-                    <span className={`text-xs pb-0.5 font-semibold ${!isClicked ? "text-black-600" : "text-gray-500"}`}>4·Review</span>
-                </button>
+            <div className="md:px-35 px-4">
+                <div className="flex gap-3 md:gap-[32.2px] items-center pt-7.5 pb-10">
+                    <button className="flex flex-col flex-1 gap-1 items-center border-b" onClick={() => handleToggleClick("course")}>
+                        <img src={isClicked ? "/images/icons/ico-course_active.svg" : "/images/icons/ico-course_default.svg"} alt="Course" />
+                        <span className={`text-xs pb-0.5 font-semibold ${isClicked ? "text-black-600" : "text-gray-500"}`}>3·Course</span>
+                    </button>
+                    <button className="flex flex-col flex-1 gap-1 items-center border-b" onClick={() => handleToggleClick("review")}>
+                        <img src={isClicked ? "/images/icons/ico-review_default.svg" : "/images/icons/ico-review_active.svg"} alt="Review" />
+                        <span className={`text-xs pb-0.5 font-semibold ${!isClicked ? "text-black-600" : "text-gray-500"}`}>4·Review</span>
+                    </button>
+                </div>
+                {isClicked ? <CourseLayout /> : <ReviewLayout />}
             </div>
-            {isClicked ? <CourseLayout /> : <ReviewLayout />}
         </>
     );
 }
